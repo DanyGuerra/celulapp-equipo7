e7249ede37d105e7fc534a82c00e81369b6979c9
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-10-16 19:56:29 -0500

    Iterando los Slides del carousel

--- src/components/Exhibitor.js
+++ src/components/Exhibitor.js
@@ -2,47 +2,43 @@ import React from "react";
 import { Carousel } from "react-bootstrap";
 import "../css/Exhibitor.css";
 
+const slides = [
+  {
+    title: "Nuevo Iphone 13",
+    description: "Compra el nuevo Iphone 13",
+    src: "https://www.altonivel.com.mx/wp-content/uploads/2019/09/iphone-11-4.jpg",
+  },
+  {
+    title: "Mas vendidos",
+    description: "Hechale un vistaso a los celulares mas vendidos del 2021",
+    src: "https://images.clarin.com/2021/01/14/samsung-galaxy-s21-el-buque___PaFSL3726_1200x630__1.jpg",
+  },
+  {
+    title: "Mejor calificados",
+    description: "Los celulares con mejores calificaciones",
+    src: "https://www.unidiversidad.com.ar/cache/celulares-gama-alta-el-pais_1000_1100.jpg",
+  },
+];
+
 function Exhibitor() {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.altonivel.com.mx/wp-content/uploads/2019/09/iphone-11-4.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.clarin.com/2021/01/14/samsung-galaxy-s21-el-buque___PaFSL3726_1200x630__1.jpg"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.unidiversidad.com.ar/cache/celulares-gama-alta-el-pais_1000_1100.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div>
+      <Carousel>
+        {slides.map((slide, index) => (
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={slide.src}
+              alt={`Slide ${index}`}
+            />
+            <Carousel.Caption>
+              <h3>{slide.title}</h3>
+              <p>{slide.description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 }
 
