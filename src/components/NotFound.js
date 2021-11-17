593f299d158c996ddaf61f219b35b040060ae2ed
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-11-17 13:56:40 -0600

    Component not found funcionando

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,24 +1,27 @@
 import { Link } from "react-router-dom";
-// import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import "../css/NotFound.css";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const NotFound = () => {
   return (
-    <>
+    <div className="not-found">
       <h1>Página no encontrada</h1>
-      <h1>
-        <Link
-          to="/"
-          style={{
-            color: "black",
-            textDecoration: "none",
-            fontSize: "30px",
-          }}
-        >
-          {/* <ArrowBackIcon fontSize="Medium"></ArrowBackIcon> */}
-          Ir a inicio
-        </Link>
-      </h1>
-    </>
+      <div className="not-found-go-home">
+        <h1>
+          <Link
+            to="/"
+            style={{
+              color: "black",
+              textDecoration: "none",
+              fontSize: "30px",
+            }}
+          >
+            <ArrowBackIcon fontSize="Medium"></ArrowBackIcon>
+            Ir a inicio
+          </Link>
+        </h1>
+      </div>
+    </div>
   );
 };
 
