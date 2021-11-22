8c25f81956fd27a27f8dc9d38706da1b75af077a
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-11-21 18:43:35 -0600

    Filtro todos y por marca de celular

--- src/components/DropdownPhone.js
+++ src/components/DropdownPhone.js
@@ -1,22 +1,48 @@
 import "../css/DropdownPhone.css";
-
+import { Link } from "react-router-dom";
 // import { useHistory } from "react-router";
 
 const DropdownPhone = (props) => {
   // const history = useHistory();
 
+  const closeDropPhone = () => {
+    props.setShowDropPhone(false);
+  };
+
   if (props.showDropPhone) {
     return (
-      <div className="dropdown-phone">
+      <div
+        className="dropdown-phone"
+        onMouseLeave={() => {
+          props.setShowDropPhone(false);
+        }}
+      >
         <div className="todos">
-          <div className="title">Todos</div>
+          <Link to="/todos-los-celulares">
+            <div className="title" onClick={closeDropPhone}>
+              Todos
+            </div>
+          </Link>
           <div className="mas-filtros">Mas Filtros</div>
         </div>
         <div className="col">
           <div className="title">Marcas</div>
-          <div className="item"> Xiaomi</div>
-          <div className="item"> Apple</div>
-          <div className="item"> Samsung</div>
+          <div className="item" onClick={closeDropPhone}>
+            <Link to="/marca/xiaomi" className="item">
+              Xiaomi
+            </Link>
+          </div>
+          <div className="item" onClick={closeDropPhone}>
+            <Link to="/marca/apple" className="item">
+              Apple
+            </Link>
+          </div>
+          <div className="item" onClick={closeDropPhone}>
+            <Link to="/marca/samsung" className="item">
+              Samsung
+            </Link>
+          </div>
+          <div className="mas-filtros"> Todas las marcas</div>
         </div>
         <div className="col">
           <div className="title">Sistema Operativo</div>
