066a1cc64b275f768cebcd7dc4292dc939fb5fb0
Orozcastro <47483805+Orozcastro@users.noreply.github.com>
2021-12-14 05:48:06 -0600

    Se arreglo el diseño de la lista de celulares

--- src/components/Celular.js
+++ src/components/Celular.js
@@ -18,29 +18,35 @@ function Celular({ marca, modelo, images, precio, _id }) {
   }
 
   return (
-    <Container>
-      <Toast style={{ width: "28rem", height: "532px" }} className="m-3 p-3">
+    <Container className="Container">
+      <Toast
+        style={{ width: "19rem", height: "28rem" }}
+        className="Toast m-3 p-3"
+      >
         <Row className="justify-content-md-center Row ">
           <Card.Img
             style={{
-              maxHeight: "20rem",
+              maxHeight: "15rem",
               width: "auto",
               textAlign: "center",
             }}
             src={images[0]}
             alt={modelo}
           />
-          <Card.Title as="h2" className="text-center">
+          <Card.Title as="h2" className="text-center Title">
             {firstUpper(marca)}
           </Card.Title>
-          <Card.Subtitle as="h5" className="text-center mb-1 text-muted">
+          <Card.Subtitle
+            as="h5"
+            className="text-center mb-1 text-muted Subtitle"
+          >
             {firstUpper(modelo)}
           </Card.Subtitle>
           <span className="precio text-center">$ {precio}</span>
           <Button
-            style={{ width: "20rem" }}
+            style={{ width: "80%" }}
             variant="primary"
-            className="text-center btn-detalle m-0"
+            className="text-center btn-detalle m-0 Button"
             onClick={(e) => handleDetalles(e)}
           >
             Detalles
